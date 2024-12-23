6a42c8c0c497e939cee3c34caa2e747c9d321740
stepheeeen <stephenon56@gmail.com>
2024-12-23 19:07:53 +0100

    styling authentication

--- src/components/Auth/SignInForm.tsx
+++ src/components/Auth/SignInForm.tsx
@@ -1,18 +1,28 @@
 import Button from "../ui/custom/button";
 import InputField from "../ui/InputField";
 
-
 const SignInForm = () => (
-  <div className="p-6 bg-white rounded-md max-w-md mx-auto">
-    <h2 className="text-2xl font-bold mb-4">Welcome Back</h2>
-    <InputField type="email" placeholder="Email" />
-    <InputField type="password" placeholder="Password" />
-    <a href="/forgot-password" className="text-sm text-yellow-400 float-right mb-4">Forgot Password?</a>
+  <div className="p-6 bg-white rounded-md w-full md:max-w-md mx-auto h-full md:h-auto">
+    <h2 className="text-3xl font-bold mb-1 mx-1">Welcome Back</h2>
+    <p className="mb-6 md:mb-3 mx-1">login into your account.</p>
+    <form className="flex flex-col gap-4 w-full">
+      <InputField type="email" placeholder="Email" />
+      <InputField type="password" placeholder="Password" />
+      <a
+        href="/Authentication/Forget-password"
+        className="text-sm text-yellow-400 text-right mb-4"
+      >
+        Forgot Password?
+      </a>
+    </form>
     <Button label="Proceed" />
-    <div className="text-center mt-4">Or</div>
+    <div className="text-center my-1 text-base">Or</div>
     <Button label="Continue With Google" variant="secondary" />
     <p className="text-center text-sm mt-4">
-      Don't Have An Account? <a href="/signup" className="text-yellow-400">Sign Up</a>
+      Don't Have An Account?{" "}
+      <a href="/Authentication/Signup" className="text-yellow-400">
+        Sign Up
+      </a>
     </p>
   </div>
 );
