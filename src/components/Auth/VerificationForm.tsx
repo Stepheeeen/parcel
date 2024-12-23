a63adb17d073b5371cf5a9bc1a583537726070f1
stepheeeen <stephenon56@gmail.com>
2024-12-23 21:26:15 +0100

    `Refactor OTP input and layout in ForgotPasswordForm and VerificationForm components`

--- src/components/Auth/VerificationForm.tsx
+++ src/components/Auth/VerificationForm.tsx
@@ -7,16 +7,16 @@ import {
 } from "@/components/ui/input-otp";
 
 const VerificationForm = () => (
-  <div className="p-6 bg-white rounded-md max-w-md mx-auto shadow-md">
-    <h2 className="text-2xl font-bold mb-4">Email Verification</h2>
-    <p className="text-sm text-gray-500 mb-4">
-      A 6-digit code was sent to your email.
+  <div className="p-6 bg-white rounded-md max-w-md mx-auto shadow-none md:shadow-md">
+    <h2 className="text-2xl font-bold mb-1 mx-1">Email Verification</h2>
+    <p className="mb-9 text-base mx-1 text-gray-500">
+      A 6 digit code was sent to the address roonie**********@gmail.com
     </p>
-    <div className="flex justify-between gap-2 mb-6">
+    <div className="flex justify-between items-center w-full gap-3 mb-14">
       <InputOTP maxLength={6}>
         {[0, 1, 2, 3, 4, 5].map((index) => (
           <InputOTPGroup key={`group-1-${index}`}>
-            <InputOTPSlot index={index} className="p-6" />
+            <InputOTPSlot index={index} className="p-7 text-2xl" />
           </InputOTPGroup>
         ))}
       </InputOTP>
