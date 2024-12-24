6b46fb1a94c34f3e335f7588feb32e5830f9fde3
Utee Ekong <utibeabasiekong2@gmail.com>
2024-12-24 13:28:48 +0100

    update: change date format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,34 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getDate = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const dayName = days[date.getDay()];
   const day = String(date.getDate()).padStart(2, "0");
-  return `${day}-${month}-${year}`;
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${dayName}, ${day} ${month}, ${year}`;
 };
