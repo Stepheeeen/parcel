559299c45f6d5da25f39e40e9180d46419d9e47c
Utee Ekong <utibeabasiekong2@gmail.com>
2024-12-26 15:39:45 +0100

    update: link buttons on homepage to the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import {
   Bell,
@@ -12,8 +13,12 @@ import {
   Phone,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const LandingPage = () => {
+
+  const router = useRouter();
+
   const features = [
     {
       icon: <Clock className="w-8 h-8 text-yellow-500" />,
@@ -111,7 +116,7 @@ const LandingPage = () => {
                 Contact
               </a>
             </div>
-            <Button className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold shadow-lg hover:shadow-xl transition-all">
+            <Button className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold shadow-lg hover:shadow-xl transition-all" onClick={()=>router.push("/authentication/signup/rider")}>
               Become a Rider
             </Button>
           </div>
@@ -133,14 +138,14 @@ const LandingPage = () => {
                 with Parcel.
               </p>
               <div className="mt-8 sm:mt-10 sm:flex sm:justify-center lg:justify-start space-x-4">
-                <Button className="bg-yellow-500 hover:bg-yellow-600 text-white px-8 py-6 text-lg font-semibold shadow-lg hover:shadow-xl transition-all">
+                <Button className="bg-yellow-500 hover:bg-yellow-600 text-white px-8 py-6 text-lg font-semibold shadow-lg hover:shadow-xl transition-all" onClick={()=> router.push("/authentication/signup/user")}>
                   Create Account
                   <ChevronRight className="ml-2 h-5 w-5" />
                 </Button>
                 <Button
                   variant="outline"
                   className="mt-3 sm:mt-0 sm:ml-3 px-8 py-6 text-lg font-semibold shadow-lg hover:shadow-xl transition-all"
-                >
+                onClick={() => router.push("/authentication/signin")}>
                   Login
                 </Button>
               </div>
