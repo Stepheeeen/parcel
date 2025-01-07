84b175b0efc6293f02fa5455481732af548f9cbc
stepheeeen <stephenon56@gmail.com>
2025-01-07 13:02:56 +0100

    feat: implement custom button component and update landing page buttons

--- src/components/ui/custom/button.tsx
+++ src/components/ui/custom/button.tsx
@@ -1,19 +1,26 @@
 interface ButtonProps {
-    label: any;
-    onClick?: any;
-    variant?: "primary" | "secondary";
-    disabled?: any;
-  }
-  
-  const Button = ({ label, onClick, variant = "primary", disabled }: ButtonProps) => (
-    <button
-      onClick={onClick}
-      className={`w-full py-3 rounded-full font-medium ${
-        variant === "primary" ? "bg-[#F9CA44] hover:bg-yellow-500 text-white text-lg" : "text-black text-lg outline outline-1 outline-[#F9CA44]"
-      }`}
-    >
-      {label}
-    </button>
-  );
-  
-  export default Button;
+  label: any;
+  onClick?: any;
+  variant?: "primary" | "secondary";
+  disabled?: any;
+}
+
+const Button = ({
+  label,
+  onClick,
+  variant = "primary",
+  disabled,
+}: ButtonProps) => (
+  <button
+    onClick={onClick}
+    className={`w-full py-3 rounded-full font-semibold ${
+      variant === "primary"
+        ? "bg-[#F9CA44] hover:bg-yellow-500 text-white text-lg"
+        : "text-black text-lg outline outline-1 outline-[#F9CA44]"
+    }`}
+  >
+    {label}
+  </button>
+);
+
+export default Button;
